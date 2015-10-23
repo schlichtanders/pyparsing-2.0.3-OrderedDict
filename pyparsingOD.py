50bb1a10b63ac623ef58ffa3ee59bb08be172ff4
Stephan Sahm <Stephan.Sahm@gmx.de>
2015-10-23 13:03:14 +0200

    version must not be suffixed

--- pyparsingOD.py
+++ pyparsingOD.py
@@ -57,7 +57,7 @@ The pyparsing module handles some of the problems that are typically vexing when
  - embedded comments
 """
 
-__version__ = "2.0.3.1.OD"
+__version__ = "2.0.3.1"
 __versionTime__ = "6 Sep 2015 13:00"
 __author__ = "Paul McGuire <ptmcg@users.sourceforge.net>, Stephan Sahm <stephan.sahm@gmx.de"
 
